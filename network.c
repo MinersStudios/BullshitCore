c8eafd0bd5df051558befb08c8650ab0c23d6af6
PackmanDude <packmandude@protonmail.com>
2024-04-10 18:36:20 +0300

    Disable delay before sending data

--- network.c
+++ network.c
@@ -1,4 +1,5 @@
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,12 +11,16 @@
 #define ADDRESS INADDR_ANY
 #define PORT 25565
 #define PERROR_AND_GOTO_CLOSEFD(s, ctx) { perror(s); goto ctx ## closefd; }
+#define MINECRAFT_VERSION "1.20.4"
+#define PROTOCOL_VERSION 765
 
 // TODO: Add support for IPv6
 int main(void)
 {
 	int server_endpoint = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (unlikely(server_endpoint == -1)) PERROR_AND_EXIT("socket")
+	if (unlikely(setsockopt(server_endpoint, IPPROTO_TCP, TCP_NODELAY, &(const int){ 1 }, sizeof(int)) == -1))
+		PERROR_AND_GOTO_CLOSEFD("setsockopt", server)
 	{
 		const struct sockaddr_in server_address = { AF_INET, htons(PORT), (struct in_addr){ htonl(ADDRESS) } };
 		struct sockaddr server_address_data;
