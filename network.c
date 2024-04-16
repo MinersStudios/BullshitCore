1f33b9e854fed5bb6ab1727e0a3d1d8671901d9b
PackmanDude <packmandude@protonmail.com>
2024-04-17 00:01:02 +0300

    Implement variable-length data encodings

--- network.c
+++ network.c
@@ -2,6 +2,7 @@
 #include <netinet/in.h>
 #include <netinet/tcp.h>
 #include <pthread.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,12 +18,57 @@
 // config
 #define ADDRESS "0.0.0.0"
 #define PORT 25565
-#define MAX_PLAYERS 15 // cannot be larger than 32767
+#define MAX_CONNECTIONS 15 // cannot be larger than 32767
 
-void *main_routine(void *p_player)
+VarInt varint_encode(int32_t value)
 {
-	int player = *(int *)p_player;
-	free(p_player);
+	return varlong_encode((int64_t)value);
+}
+
+int32_t varint_decode(VarInt varint)
+{
+	int32_t value = 0;
+	for (size_t i = 0; i < 5; ++i)
+	{
+		const int8_t varint_byte = varint[i];
+		value |= (varint_byte & 0x7F) << i * 7;
+		if (!(varint_byte & 0x80)) break;
+	}
+	return value;
+}
+
+VarLong varlong_encode(int64_t value)
+{
+	uint_fast8_t bytes = 1;
+	while (value >> 7 * bytes) bytes += !!(value >> 7);
+	VarLong varlong = calloc(bytes, 1);
+	if (unlikely(!varlong)) return NULL;
+	for (size_t i = 0; i < bytes; ++i)
+		varlong[i] = value >> 7 * i & 0x7F | (i != bytes - 1u) << 7;
+	return varlong;
+}
+
+int64_t varlong_decode(VarLong varlong)
+{
+	int64_t value = 0;
+	for (size_t i = 0; i < 10; ++i)
+	{
+		const int8_t varlong_byte = varlong[i];
+		value |= (varlong_byte & 0x7F) << 7 * i;
+		if (!(varlong_byte & 0x80)) break;
+	}
+	return value;
+}
+
+static void *main_routine(void *p_client_endpoint)
+{
+	int client_endpoint = *(int *)p_client_endpoint;
+	free(p_client_endpoint);
+	Boolean compression_enabled = false;
+	enum State current_state;
+	Byte buffer[128];
+	// int bytes_read = recv(client_endpoint, &buffer, sizeof buffer, 0);
+	// if (unlikely(bytes_read == -1)) return (void *)1;
 	return NULL;
 }
 
@@ -57,12 +103,12 @@ int main(void)
 		int client_endpoint;
 		{
 			int_least16_t thread_counter = 0;
-			while (true)
+			while (1)
 			{
 				client_endpoint = accept(server_endpoint, NULL, NULL);
 				if (unlikely(client_endpoint == -1))
 					PERROR_AND_GOTO_CLOSEFD("accept", server)
-				if (thread_counter == MAX_PLAYERS)
+				if (thread_counter == MAX_CONNECTIONS)
 				{
 					if (unlikely(close(client_endpoint) == -1))
 						PERROR_AND_GOTO_CLOSEFD("close", server)
