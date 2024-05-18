5217884a08c025ed8660f3f5fcec8c733db3dfa0
PackmanDude <packmandude@protonmail.com>
2024-05-18 20:48:41 +0300

    Simplify build script

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-script_dir="$(dirname "$0")"
+(cd "$(dirname "$0")"
 gcc -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow -Wwrite-strings \
 -Wstrict-prototypes -Wold-style-definition -Wredundant-decls -Wnested-externs \
 -Wmissing-include-dirs -Wjump-misses-init -Wlogical-op -O2 -flto -pthread \
-"$script_dir"/network.c "$script_dir"/log.c "$script_dir"/main.c -o \
-"$script_dir"/a.out
+network.c log.c main.c)
