517707bfc4ae614d89ebaf88aede66f5481b20f5
PackmanDude <packmandude@protonmail.com>
2024-06-21 00:27:03 +0300

    Make release builds by default

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@
 gcc -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow -Wwrite-strings \
 -Wstrict-prototypes -Wold-style-definition -Wredundant-decls -Wnested-externs \
 -Wmissing-include-dirs -Wjump-misses-init -Wlogical-op -I../include -I. -O2 \
--flto -pthread network.c log.c ../main.c -o ../BullshitCore $@)
+-flto -pthread -DNDEBUG network.c log.c ../main.c -o ../BullshitCore $@)
