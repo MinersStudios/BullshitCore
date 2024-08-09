e325f7212a34222e0605b0c03d63cca7e27a2c91
PackmanDude <packmandude@protonmail.com>
2024-08-09 03:50:28 +0300

    Improve build script

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-(cd "$(dirname "$0")/source"
-. ../version
+(cd "$(dirname "$0")/source" || exit
+. ../version || { printf "Version formatted wrong\n"; exit; }
 output="../BullshitCore-$MINECRAFT_VERSION-$VERSION"
 gcc -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow -Wwrite-strings \
 -Wstrict-prototypes -Wold-style-definition -Wredundant-decls -Wnested-externs \
 -Wmissing-include-dirs -Wjump-misses-init -Wlogical-op -I../include -I. \
 -lwolfssl -O2 -flto -pthread -DNDEBUG network.c log.c world.c nbt.c ../main.c \
--o "$output" $@ && strip "$output")
+-o "$output" "$@" && strip "$output")
