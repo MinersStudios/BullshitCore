1c15604fb6b9d07c5e4c2aba02ac2b7e34cfc9d4
PackmanDude <packmandude@protonmail.com>
2024-08-23 22:12:12 +0300

    Disable LTO partitioning

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@
 output="../BullshitCore-$MINECRAFT_VERSION-$VERSION"
 gcc -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow -Wwrite-strings \
 -Wstrict-prototypes -Wold-style-definition -Wredundant-decls -Wnested-externs \
--Wmissing-include-dirs -Wjump-misses-init -Wlogical-op -I../include -I. \
--O2 -flto -pthread -DNDEBUG network.c log.c world.c nbt.c ../main.c \
--o "$output" "$@" -lwolfssl && strip "$output")
+-Wmissing-include-dirs -Wjump-misses-init -Wlogical-op -I../include -I. -O2 \
+-flto -flto-partition=one -fwhole-program -pthread -DNDEBUG network.c log.c \
+world.c nbt.c ../main.c -o "$output" "$@" -lwolfssl && strip "$output")
