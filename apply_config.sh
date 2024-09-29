2f204008a965cb1775393ece20894f9736b2b344
PackmanDude <packmandude@protonmail.com>
2024-09-29 14:03:48 +0300

    Add newlines

--- apply_config.sh
+++ apply_config.sh
@@ -2,8 +2,8 @@
 
 (set -u
 cd "$(dirname "$0")" || exit 1
-. ./version || { printf "Version formatted wrong" >&2; exit 1; }
-bin="BullshitCore-$MINECRAFT_VERSION-$VERSION" || { printf "Version formatted wrong" >&2; exit 1; }
+. ./version || { printf "Version formatted wrong\n" >&2; exit 1; }
+bin="BullshitCore-$MINECRAFT_VERSION-$VERSION" || { printf "Version formatted wrong\n" >&2; exit 1; }
 modification_dates="$(stat -c %y config.h "$bin")"
 if [ "$(printf "%s\n" "$modification_dates" | sort -nr | head -1)" = "$(printf "%s\n" "$modification_dates" | head -1)" ]
 then ./build.sh
