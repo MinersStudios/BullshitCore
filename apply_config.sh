52f5385bad82e4261ec582ae1c6f5705b318568a
PackmanDude <packmandude@protonmail.com>
2024-09-29 09:55:50 +0300

    Improve shell scripts

--- apply_config.sh
+++ apply_config.sh
@@ -4,6 +4,7 @@
 cd "$(dirname "$0")" || exit 1
 . ./version || { printf "Version formatted wrong" >&2; exit 1; }
 bin="BullshitCore-$MINECRAFT_VERSION-$VERSION" || { printf "Version formatted wrong" >&2; exit 1; }
-if [ "$(stat -c %y "$bin" config.h | sort -nr | head -1)" = "$(stat -c %y config.h)" ]; then
-	./build.sh
+modification_dates="$(stat -c %y config.h "$bin")"
+if [ "$(printf "%s\n" "$modification_dates" | sort -nr | head -1)" = "$(printf "%s\n" "$modification_dates" | head -1)" ]
+then ./build.sh
 fi)
