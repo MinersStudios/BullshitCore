223d76a2501486ad9b27475873c2199dba4fbbb4
PackmanDude <packmandude@protonmail.com>
2024-10-20 16:34:36 +0300

    Add debug build option in build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,20 @@ cd "$(dirname "$0")/source" || exit 1
 . ../version || { printf "Version formatted wrong\n" >&2; exit 1; }
 output="../BullshitCore-$MINECRAFT_VERSION-$VERSION" ||
 { printf "Version formatted wrong\n" >&2; exit 1; }
-gcc -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow -Wwrite-strings \
--Wstrict-prototypes -Wold-style-definition -Wredundant-decls -Wnested-externs \
--Wmissing-include-dirs -Wjump-misses-init -Wlogical-op -I../include -I. -I.. \
--O2 -flto -flto-partition=one -fwhole-program -pthread -DNDEBUG ./*.c \
-../main.c -o "$output" "$@" -lwolfssl && strip "$output")
+set +u
+case $1 in
+	debug)
+		shift
+		gcc -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow \
+		-Wwrite-strings -Wstrict-prototypes -Wold-style-definition \
+		-Wredundant-decls -Wnested-externs -Wmissing-include-dirs \
+		-Wjump-misses-init -Wlogical-op -I../include -I. -I.. -g -pthread \
+		./*.c ../main.c -o "$output-debug" "$@" -lwolfssl;;
+	*)
+		gcc -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow \
+		-Wwrite-strings -Wstrict-prototypes -Wold-style-definition \
+		-Wredundant-decls -Wnested-externs -Wmissing-include-dirs \
+		-Wjump-misses-init -Wlogical-op -I../include -I. -I.. -O2 -flto \
+		-flto-partition=one -fwhole-program -pthread -DNDEBUG ./*.c ../main.c \
+		-o "$output" "$@" -lwolfssl && strip "$output";;
+esac)
