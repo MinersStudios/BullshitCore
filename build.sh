53d50cbd74a0d01d417c2680193b141d57be7b65
PackmanDude <packmandude@protonmail.com>
2024-10-21 10:12:59 +0300

    Add version to library's name

--- build.sh
+++ build.sh
@@ -3,37 +3,37 @@
 (set -u
 cd "$(dirname "$0")/source" || exit 1
 . ../version || { printf "Version formatted wrong\n" >&2; exit 1; }
-output="../BullshitCore-$MINECRAFT_VERSION-$VERSION" ||
+app="../BullshitCore-$MINECRAFT_VERSION-$VERSION" ||
 { printf "Version formatted wrong\n" >&2; exit 1; }
+lib="../libbullshitcore-$MINECRAFT_VERSION-$VERSION"
 set +u
 case $1 in
 	debug)
 		shift
-		gcc -c -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow \
+		gcc -c -std=c99 -fPIC -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow \
 		-Wwrite-strings -Wstrict-prototypes -Wold-style-definition \
 		-Wredundant-decls -Wnested-externs -Wmissing-include-dirs \
 		-Wjump-misses-init -Wlogical-op -I../include -I.. -g -pthread ./*.c \
 		"$@"
-		gcc -std=c99 -fPIC -shared -g -pthread ./*.o -o \
-		../libbullshitcore-debug.so "$@" -lwolfssl
+		gcc -std=c99 -fPIC -shared -g -pthread ./*.o -o "$lib-debug.so" "$@" \
+		-lwolfssl
 		gcc -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow \
 		-Wwrite-strings -Wstrict-prototypes -Wold-style-definition \
 		-Wredundant-decls -Wnested-externs -Wmissing-include-dirs \
 		-Wjump-misses-init -Wlogical-op -I../include -I. -g -pthread ./*.o \
-		../main.c -o "$output-debug" "$@" -lwolfssl;;
+		../main.c -o "$app-debug" "$@" -lwolfssl;;
 	*)
-		gcc -c -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow \
+		gcc -c -std=c99 -fPIC -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow \
 		-Wwrite-strings -Wstrict-prototypes -Wold-style-definition \
 		-Wredundant-decls -Wnested-externs -Wmissing-include-dirs \
 		-Wjump-misses-init -Wlogical-op -I../include -I.. -O2 -flto \
 		-fno-fat-lto-objects -pthread -DNDEBUG ./*.c "$@"
 		gcc -std=c99 -fPIC -shared -O2 -flto -flto-partition=one -pthread \
-		./*.o -o ../libbullshitcore.so "$@" -lwolfssl
+		./*.o -o "$lib.so" "$@" -lwolfssl
 		gcc -std=c99 -Wall -Wextra -Wpedantic -Wformat=2 -Wshadow \
 		-Wwrite-strings -Wstrict-prototypes -Wold-style-definition \
 		-Wredundant-decls -Wnested-externs -Wmissing-include-dirs \
 		-Wjump-misses-init -Wlogical-op -I../include -I. -O2 -flto \
 		-flto-partition=one -fno-fat-lto-objects -fwhole-program -pthread \
-		-DNDEBUG ./*.o ../main.c -o "$output" "$@" -lwolfssl && strip "$output"
-		;;
+		-DNDEBUG ./*.o ../main.c -o "$app" "$@" -lwolfssl && strip "$app";;
 esac)
